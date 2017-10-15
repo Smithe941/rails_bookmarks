2d8cafe407389dfa6d3e021f43a3217af5e945b9
smithe <onupriy94@gmail.com>
2017-10-16 01:18:48 +0300

    fix form

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,8 @@ class Bookmark < ApplicationRecord
   belongs_to :user
   paginates_per 8
 
+  validates_presence_of :title, :url
+
   def self.search(query)
     where('url LIKE ?', "%#{query}%")
   end
